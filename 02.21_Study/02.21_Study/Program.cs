397c7fc2a2470ae8846763ff7954d99adb75394e
양사무엘 <109801997+YangSamuel726@users.noreply.github.com>
2025-02-21 09:57:14 +0900

    02.21 공부1

---
+++ 02.21_Study/02.21_Study/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._21_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("깃허브 연결하기");
+        }
+    }
+}
