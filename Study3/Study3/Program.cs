3fb595540e0c3a98bcaad7c5e10d562a399cc7aa
양사무엘 <109801997+YangSamuel726@users.noreply.github.com>
2025-02-21 10:50:05 +0900

    Study3

---
+++ Study3/Study3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float hp = 100f;
+            float att = 56.7f;
+            string name = "Yangsamuel";
+            char level = 'S';
+
+            WriteLine("캐릭터");
+            WriteLine(hp);
+            WriteLine(att);
+            WriteLine(name);
+            WriteLine(level);
+        }
+    }
+}
